b1b64ee83ee2821d65cc40b81c7d4e82c06b558f
newbiz <newbiz.uangel@gmail.com>
2012-01-11 11:48:18 +0900

    dd

--- app/member/view/TabPanel_Role.js
+++ app/member/view/TabPanel_Role.js
@@ -3,9 +3,213 @@ Ext.define('ProductCatalog.Member.view.TabPanel_Role', {
     alias: 'widget.Member.TabPanel_Role',
     id:'TabPanel_Role',
     title: 'Role',
-
+    layout:{
+    	type:'hbox'
+    },
     initComponent: function() {
         var me = this;
+
+        Ext.define('ProductCatalog.Member.Role', {
+            extend: 'Ext.data.Model',
+            fields: [
+                {name: 'id'},
+				{name: 'name'},
+				{name: 'description'}
+            ]
+        });
+
+        var myStore = Ext.create('Ext.data.Store', {
+        	proxy:'memory',
+        	model:'ProductCatalog.Member.Role',
+            data: [
+		        {
+		            id: '1',
+		            name: 'admin',
+		            description:'administrator role',
+		        },
+		        {
+		            id: '2',
+		            name: 'operator',
+		            description:'operator role',
+		        },
+		        {
+		            id: '3',
+		            name: 'agent',
+		            description:'agent role',
+		        }
+            ]
+        });
+
+//        var rowEditing = Ext.create('Ext.grid.plugin.RowEditing');
+        var rowEditing = Ext.create('Ext.grid.plugin.RowEditing', {
+            clicksToMoveEditor: 1,
+            autoCancel: false
+        });
+
+        Ext.applyIf(me, {
+            items: [
+                {
+                	xtype: 'gridpanel',
+                    frameHeader: false,
+                    forceFit:true,
+                    flex:1,
+                    columns: [
+                        {
+                            xtype: 'gridcolumn',
+                            dataIndex: 'id',
+                            text: 'ID',
+                            editor: {
+                                allowBlank: false
+                            }
+                        },
+                        {
+                            xtype: 'gridcolumn',
+                            dataIndex: 'name',
+                            text: 'Name',
+                            editor: {
+                                allowBlank: false
+                            }
+                        },
+                        {
+                            xtype: 'gridcolumn',
+                            dataIndex: 'description',
+                            text: 'Description',
+                            editor: {
+                                allowBlank: false
+                            }
+                        },
+                    ],
+                    viewConfig: {
+
+                    },
+                    dockedItems: [
+	                      {
+	                          xtype: 'toolbar',
+	                          dock: 'top',
+	                          items: [
+	                              {
+	                                  xtype: 'button',
+	                                  text: 'Add',
+	                                  iconCls:'icon-add',
+	                                  handler: function(){
+	                                      // empty record
+	                                	  myStore.insert(0, {});
+	                                      rowEditing.startEdit(0, 0);
+	                                  }
+	                              },'-',
+	                              {
+	                            	  xtype: 'button',
+	                                  text: 'Delete',
+	                                  iconCls:'icon-delete',
+	                                  handler: function() {
+	                                	  var sm = me.items.items[0].getSelectionModel();
+	                                      rowEditing.cancelEdit();
+	                                      myStore.remove(sm.getSelection());
+	                                      if (myStore.getCount() > 0) {
+	                                          sm.select(0);
+	                                      }
+	                                  }
+	                              }
+	                          ]
+	                      }
+	                  ],
+	                  store:myStore,
+	                  plugins: [rowEditing],
+
+                },
+                {
+                	xtype: 'form',
+                    flex:1,
+//                    margin: '0px 0px 0px 10px',
+                    padding: '0px 0px 0px 10px',
+                    items: [{
+                                xtype: 'label',
+                                text: 'Product Desginer',
+                                anchor: '100%'
+                            },
+
+                            {
+                                xtype: 'checkboxfield',
+                                fieldLabel: 'Editing',
+                                boxLabel: '',
+                                anchor: '100%'
+                            },
+                            {
+                                xtype: 'checkboxfield',
+                                fieldLabel: 'Requested',
+                                boxLabel: '',
+                                anchor: '100%'
+                            },
+                            {
+                                xtype: 'checkboxfield',
+                                fieldLabel: 'Activated',
+                                boxLabel: '',
+                                anchor: '100%'
+                            },
+                            {
+                                xtype: 'checkboxfield',
+                                fieldLabel: 'Deprecated',
+                                boxLabel: '',
+                                anchor: '100%'
+                            },
+                            {
+                                xtype: 'checkboxfield',
+                                fieldLabel: 'Closed',
+                                boxLabel: '',
+                                anchor: '100%'
+                            },
+                            {
+                                xtype: 'checkboxfield',
+                                fieldLabel: 'Conditions',
+                                boxLabel: '',
+                                anchor: '100%'
+                            },
+                            {
+                                xtype: 'checkboxfield',
+                                fieldLabel: 'Actions',
+                                boxLabel: '',
+                                anchor: '100%'
+                            },
+                            {
+                                xtype: 'checkboxfield',
+                                fieldLabel: 'Balances',
+                                boxLabel: '',
+                                anchor: '100%'
+                            },
+                            {
+                                xtype: 'checkboxfield',
+                                fieldLabel: 'Charging Items',
+                                boxLabel: '',
+                                anchor: '100%'
+                            },
+                            {
+                                xtype: 'checkboxfield',
+                                fieldLabel: 'Promotion Designer',
+                                boxLabel: '',
+                                anchor: '100%'
+                            },
+                            {
+                                xtype: 'checkboxfield',
+                                fieldLabel: 'Relation Manager',
+                                boxLabel: '',
+                                anchor: '100%'
+                            },
+                            {
+                                xtype: 'checkboxfield',
+                                fieldLabel: 'Lifecycle Manager',
+                                boxLabel: '',
+                                anchor: '100%'
+                            },
+                            {
+                                xtype: 'checkboxfield',
+                                fieldLabel: 'Member Manager',
+                                boxLabel: '',
+                                anchor: '100%'
+                            }
+                        ]
+                },
+            ],
+        });
 
         me.callParent(arguments);
     }
--- app/member/view/TabPanel_User.js
+++ app/member/view/TabPanel_User.js
@@ -7,15 +7,20 @@ Ext.define('ProductCatalog.Member.view.TabPanel_User', {
     initComponent: function() {
         var me = this;
 
+        Ext.define('ProductCatalog.Member.User', {
+            extend: 'Ext.data.Model',
+            fields: [
+				{name: 'id'},
+				{name: 'name'},
+				{name: 'email'},
+				{name: 'phonenumber'},
+				{name: 'role'},
+            ]
+        });
+
         var myStore = Ext.create('Ext.data.Store', {
         	proxy:'memory',
-        	fields:[
-	           {name: 'id'},
-	           {name: 'name'},
-	           {name: 'email'},
-	           {name: 'phonenumber'},
-	           {name: 'role'},
-	        ],
+        	model:'ProductCatalog.Member.User',
             data: [
 		        {
 		            id: 'realjangsun',
@@ -28,17 +33,23 @@ Ext.define('ProductCatalog.Member.view.TabPanel_User', {
 		            id: 'youngmin',
 		            name: 'Youngmin Kim',
 		            email:'youngmin@uangel.com',
-		            phonenumber:'8200010002',
+		            phonenumber:'821000010002',
 		            role:'operator',
 		        },
 		        {
 		            id: 'james',
 		            name: 'Jame lee',
 		            email:'jameslee@yourock.com',
-		            phonenumber:'8200010003',
+		            phonenumber:'821000010003',
 		            role:'agent',
 		        },
             ]
+        });
+
+//        var rowEditing = Ext.create('Ext.grid.plugin.RowEditing');
+        var rowEditing = Ext.create('Ext.grid.plugin.RowEditing', {
+            clicksToMoveEditor: 1,
+            autoCancel: false
         });
 
         Ext.applyIf(me, {
@@ -51,27 +62,42 @@ Ext.define('ProductCatalog.Member.view.TabPanel_User', {
                         {
                             xtype: 'gridcolumn',
                             dataIndex: 'id',
-                            text: 'ID'
+                            text: 'ID',
+                            editor: {
+                                allowBlank: false
+                            }
                         },
                         {
                             xtype: 'gridcolumn',
                             dataIndex: 'name',
-                            text: 'Name'
+                            text: 'Name',
+                            editor: {
+                                allowBlank: false
+                            }
                         },
                         {
                             xtype: 'gridcolumn',
                             dataIndex: 'email',
-                            text: 'Email'
+                            text: 'Email',
+                            editor: {
+                                allowBlank: false
+                            }
                         },
                         {
                             xtype: 'gridcolumn',
                             dataIndex: 'phonenumber',
-                            text: 'Phone Number'
+                            text: 'Phone Number',
+                            editor: {
+                                allowBlank: false
+                            }
                         },
                         {
                             xtype: 'gridcolumn',
                             dataIndex: 'role',
-                            text: 'Role'
+                            text: 'Role',
+                            editor: {
+                                allowBlank: false
+                            }
                         },
                     ],
                     viewConfig: {
@@ -86,16 +112,32 @@ Ext.define('ProductCatalog.Member.view.TabPanel_User', {
 	                                  xtype: 'button',
 	                                  text: 'Add',
 	                                  iconCls:'icon-add',
-	                              },
+	                                  handler: function(){
+	                                      // empty record
+	                                	  myStore.insert(0, {});
+	                                      rowEditing.startEdit(0, 0);
+	                                  }
+	                              },'-',
 	                              {
+	                            	  itemId: 'removeUser',
 	                                  xtype: 'button',
 	                                  text: 'Delete',
 	                                  iconCls:'icon-delete',
+	                                  handler: function() {
+	                                	  var sm = me.items.items[0].getSelectionModel();
+	                                      rowEditing.cancelEdit();
+	                                      myStore.remove(sm.getSelection());
+	                                      if (myStore.getCount() > 0) {
+	                                          sm.select(0);
+	                                      }
+	                                  }
 	                              }
 	                          ]
 	                      }
 	                  ],
-	                  store:myStore
+	                  store:myStore,
+	                  plugins: [rowEditing],
+
                 }
             ],
 
